8d1191f9c2d55dd5448059b597d275f1f90de1ec
iammonali <monalisasamal1653@gmail.com>
2024-04-11 09:55:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let [seconds,minutes, hours] = [0,0,0];
+let time=document.getElementById("time");
+let timer=null;
+function stopwatch(){
+    seconds++;
+    if(seconds==60){
+        seconds=0;
+        minutes++;
+        if(minutes==60){
+            minutes=0;
+            hours++;
+        }
+    }
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+
+
+    time.innerHTML= h +":"+ m +":"+ s;
+}
+function stop(){
+    clearInterval(timer);
+}
+function reset(){
+    clearInterval(timer);
+    [seconds, minutes, hours] = [0,0,0];
+    time.innerHTML= "00:00:00";
+}
+function start(){
+    if(timer!=null){
+        clearInterval(timer);
+    }
+    timer=setInterval(stopwatch,1000);
+}
